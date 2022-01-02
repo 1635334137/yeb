b9340d8f6b0aedc6184bf1b4274543bd4fdd2050
1635334137 <18877888233@163.com>
2022-01-03 02:20:19 +0800

    权限组所有功能完成

--- yeb-server/src/main/java/com/xxxx/server/mapper/MenuRoleMapper.java
+++ yeb-server/src/main/java/com/xxxx/server/mapper/MenuRoleMapper.java
@@ -3,6 +3,7 @@ package com.xxxx.server.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 import com.xxxx.server.pojo.MenuRole;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * <p>
@@ -14,4 +15,10 @@ import com.xxxx.server.pojo.MenuRole;
  */
 public interface MenuRoleMapper extends BaseMapper<MenuRole> {
 
+    /**
+     * 批量更新角色菜单
+     * @param rid
+     * @param mids
+     */
+    Integer insertRecord(@Param("rid") Integer rid, @Param("mids") Integer[] mids);
 }
