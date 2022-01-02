3ea15a3addfb7a3fa890c510ec9d46b32593d337
1635334137 <18877888233@163.com>
2022-01-02 21:10:10 +0800

    用户菜单通过redis缓存获取功能

---
+++ yeb-server/src/main/java/com/xxxx/server/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.xxxx.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Redis配置类
+ *
+ * @author Mr.Lan
+ * @create: 2022-01-02 20:39
+ */
+@Configuration
+public class RedisConfig {
+
+
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory factory){
+        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        redisTemplate.setConnectionFactory(factory);
+        return redisTemplate;
+    }
+}
--- yeb-server/src/main/java/com/xxxx/server/service/impl/MenuServiceImpl.java
+++ yeb-server/src/main/java/com/xxxx/server/service/impl/MenuServiceImpl.java
@@ -6,8 +6,11 @@ import com.xxxx.server.pojo.Admin;
 import com.xxxx.server.pojo.Menu;
 import com.xxxx.server.service.IMenuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
@@ -25,12 +28,24 @@ public class MenuServiceImpl extends ServiceImpl<MenuMapper, Menu> implements IM
     @Autowired
     private MenuMapper menuMapper;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     /**
      * 根据用户id查询菜单列表
+     * 用户第一次查询时查数据库，后面都查缓存
+     * 注意：当用户执行菜单的更新删除操作时，要清空缓存，让用户能拿到最新的数据
      * @return
      */
     @Override
     public List<Menu> getMenusByAdminId() {
-        return menuMapper.getMenusByAdminId(((Admin) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId());
+        Integer adminId = ((Admin) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+        ValueOperations<String,Object> valueOperations = redisTemplate.opsForValue();
+        List<Menu> menus = (List<Menu>)valueOperations.get("menu_"+adminId);
+        if(CollectionUtils.isEmpty(menus)){
+            menus = menuMapper.getMenusByAdminId(adminId);
+            valueOperations.set("menu_"+adminId,menus);
+        }
+        return menus;
     }
 }
