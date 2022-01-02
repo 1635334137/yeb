5bdc28b1977ca9dca333821281a7c3e08d8fdf63
1635334137 <18877888233@163.com>
2022-01-03 06:23:23 +0800

    获取所有员工及分页功能

--- yeb-server/src/main/java/com/xxxx/server/controller/EmployeeController.java
+++ yeb-server/src/main/java/com/xxxx/server/controller/EmployeeController.java
@@ -1,8 +1,18 @@
 package com.xxxx.server.controller;
 
 
+import com.xxxx.server.pojo.Employee;
+import com.xxxx.server.pojo.RespPageBean;
+import com.xxxx.server.service.IEmployeeService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.models.auth.In;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 /**
  * <p>
@@ -13,7 +23,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-01-01
  */
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/employee/basic")
 public class EmployeeController {
 
+    @Autowired
+    private IEmployeeService employeeService;
+
+    @ApiOperation(value = "获取所有员工（分页）")
+    @GetMapping("/")
+    public RespPageBean getEmployee(@RequestParam(defaultValue = "1")Integer currentPage,
+                                    @RequestParam(defaultValue = "10")Integer size,
+                                    Employee employee, LocalDate[] beginDateScope){
+        return employeeService.getEmployeeByPage(currentPage,size,employee,beginDateScope);
+    }
 }
--- yeb-server/src/main/java/com/xxxx/server/service/IEmployeeService.java
+++ yeb-server/src/main/java/com/xxxx/server/service/IEmployeeService.java
@@ -3,6 +3,9 @@ package com.xxxx.server.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.xxxx.server.pojo.Employee;
+import com.xxxx.server.pojo.RespPageBean;
+
+import java.time.LocalDate;
 
 /**
  * <p>
@@ -14,4 +17,13 @@ import com.xxxx.server.pojo.Employee;
  */
 public interface IEmployeeService extends IService<Employee> {
 
+    /**
+     * 获取所有员工（分页）
+     * @param currentPage
+     * @param size
+     * @param employee
+     * @param beginDateScope
+     * @return
+     */
+    RespPageBean getEmployeeByPage(Integer currentPage, Integer size, Employee employee, LocalDate[] beginDateScope);
 }
