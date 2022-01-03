0e94363e1a63854babdca5d8de98de8d0bf5659e
1635334137 <18877888233@163.com>
2022-01-03 21:59:20 +0800

    邮件发送-消息队列

---
+++ yeb-mail/src/main/java/com/xxxx/mail/MailApplication.java
@@ -0,0 +1,26 @@
+package com.xxxx.mail;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * 启动类
+ *
+ * @author Mr.Lan
+ * @create: 2022-01-03 21:25
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class MailApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MailApplication.class,args);
+    }
+
+    @Bean
+    public Queue queue(){
+        return new Queue("mail.welcome");
+    }
+}
+
--- yeb-server/src/main/java/com/xxxx/server/service/impl/EmployeeServiceImpl.java
+++ yeb-server/src/main/java/com/xxxx/server/service/impl/EmployeeServiceImpl.java
@@ -10,6 +10,7 @@ import com.xxxx.server.pojo.Employee;
 import com.xxxx.server.pojo.RespBean;
 import com.xxxx.server.pojo.RespPageBean;
 import com.xxxx.server.service.IEmployeeService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
@@ -33,6 +34,9 @@ public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> i
 
     @Autowired
     private EmployeeMapper employeeMapper;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     /**
      * 获取所有员工（分页）
@@ -75,6 +79,9 @@ public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> i
         DecimalFormat decimalFormat = new DecimalFormat("##.00");
         employee.setContractTerm(Double.parseDouble(decimalFormat.format(days/365.00)));
         if(1==employeeMapper.insert(employee)){
+            //发送信息
+            Employee emp = employeeMapper.getEmployee(employee.getId()).get(0);
+            rabbitTemplate.convertAndSend("mail.welcome",emp);
             return RespBean.success("添加成功！");
         }
         return RespBean.error("添加失败！");
