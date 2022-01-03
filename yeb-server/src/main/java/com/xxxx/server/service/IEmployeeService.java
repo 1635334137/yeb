6d07c01a76a586f95568ef646b5c615447b99ab2
1635334137 <18877888233@163.com>
2022-01-03 18:32:02 +0800

    员工管理功能

--- yeb-server/src/main/java/com/xxxx/server/service/IEmployeeService.java
+++ yeb-server/src/main/java/com/xxxx/server/service/IEmployeeService.java
@@ -3,6 +3,7 @@ package com.xxxx.server.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.xxxx.server.pojo.Employee;
+import com.xxxx.server.pojo.RespBean;
 import com.xxxx.server.pojo.RespPageBean;
 
 import java.time.LocalDate;
@@ -26,4 +27,17 @@ public interface IEmployeeService extends IService<Employee> {
      * @return
      */
     RespPageBean getEmployeeByPage(Integer currentPage, Integer size, Employee employee, LocalDate[] beginDateScope);
+
+    /**
+     * 获取工号
+     * @return
+     */
+    RespBean maxWorkID();
+
+    /**
+     * 添加员工
+     * @param employee
+     * @return
+     */
+    RespBean addEmp(Employee employee);
 }
