b150c055cac7fbb4cebd6330d73c5356debafe0f
1635334137 <18877888233@163.com>
2022-01-05 02:48:47 +0800

    邮件发送-消息队列优化

--- yeb-mail/src/main/java/com/xxxx/mail/MailReceiver.java
+++ yeb-mail/src/main/java/com/xxxx/mail/MailReceiver.java
@@ -1,19 +1,27 @@
 package com.xxxx.mail;
 
+import com.rabbitmq.client.Channel;
 import com.xxxx.server.pojo.Employee;
+import com.xxxx.server.pojo.MailConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.IOException;
 import java.util.Date;
 
 /**
@@ -34,11 +42,26 @@ public class MailReceiver {
     @Autowired
     private TemplateEngine templateEngine;
 
-    @RabbitListener(queues = "mail.welcome")
-    public void handler(Employee employee){
-        MimeMessage msg = javaMailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(msg);
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @RabbitListener(queues = MailConstants.MAIL_QUEUE_NAME)
+    public void handler(Message message, Channel channel){
+        Employee employee = (Employee) message.getPayload();
+        MessageHeaders headers = message.getHeaders();
+        long tag = (long) headers.get(AmqpHeaders.DELIVERY_TAG);
+        String msgId = (String)headers.get("spring_returned_message_correlation");
+        HashOperations hashOperations = redisTemplate.opsForHash();
+
         try {
+            if(hashOperations.entries("mail_log").containsKey(msgId)){
+                LOGGER.error("消息已经被消费=============>{}",msgId);
+                //手动确认消息，tag：消息序号，multiple：是否确认多条
+                channel.basicAck(tag,false);
+                return;
+            }
+            MimeMessage msg = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(msg);
             helper.setFrom(mailProperties.getUsername());
             helper.setTo(employee.getEmail());
             helper.setSubject("入职欢迎邮件");
@@ -51,7 +74,17 @@ public class MailReceiver {
             String mail = templateEngine.process("mail", context);
             helper.setText(mail,true);
             javaMailSender.send(msg);
-        }catch (MessagingException e){
+            LOGGER.info("邮件发送成功");
+            //将消息id存入redis
+            hashOperations.put("mail_log",msgId,"OK");
+            channel.basicAck(tag,false);
+        }catch (Exception e){
+            //requeue:是否要退回到队列
+            try {
+                channel.basicNack(tag,false,true);
+            } catch (IOException ex) {
+                LOGGER.error("邮件发送失败========>{}",e.getMessage());
+            }
             LOGGER.error("邮件发送失败========>{}",e.getMessage());
         }
     }
---
+++ yeb-server/src/main/java/com/xxxx/server/config/RabbitMQConfig.java
@@ -0,0 +1,76 @@
+package com.xxxx.server.config;
+
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.xxxx.server.pojo.MailConstants;
+import com.xxxx.server.pojo.MailLog;
+import com.xxxx.server.service.IMailLogService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * RabbitMQ配置类
+ *
+ * @author Mr.Lan
+ * @create: 2022-01-05 01:41
+ */
+@Configuration
+public class RabbitMQConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQConfig.class);
+
+    @Autowired
+    private CachingConnectionFactory cachingConnectionFactory;
+
+    @Autowired
+    private IMailLogService mailLogService;
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(cachingConnectionFactory);
+        //消息确认回调，确认消息是否到达broker，data：消息唯一标识，ack：确认结果，cause：失败原因
+        rabbitTemplate.setConfirmCallback((data,ack,cause)->{
+            String msgId = data.getId();
+            if(ack){
+                LOGGER.info("{}===========>消息发送成功",msgId);
+                mailLogService.update(new UpdateWrapper<MailLog>().set("status",1).eq("msgId",msgId));
+            }else{
+                LOGGER.error("{}==========>消息发送失败",msgId);
+            }
+        });
+        /**
+         * msg:消息主题
+         * repCode:响应码
+         * repText:响应描述
+         * exchange:交换机
+         * routingkey:路由键
+         */
+        rabbitTemplate.setReturnsCallback(new RabbitTemplate.ReturnsCallback() {
+            @Override
+            public void returnedMessage(ReturnedMessage returnedMessage) {
+                LOGGER.error("{}=================>消息发送queue时失败",returnedMessage.getMessage().getBody().toString());
+            }
+        });
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Queue queue(){
+        return new Queue(MailConstants.MAIL_QUEUE_NAME);
+    }
+
+    @Bean
+    public DirectExchange directExchange(){
+        return new DirectExchange(MailConstants.MAIL_EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(){
+        return BindingBuilder.bind(queue()).to(directExchange()).with(MailConstants.MAIL_ROUTING_KEY_NAME);
+    }
+}
